1067a14db43f4f58558921211eb3548486c26d43
lancee <lancee.website@gmail.com>
2023-09-07 21:26:01 +0800

    remove-duplicates-from-sorted-array-ii

--- 80-remove-duplicates-from-sorted-array-ii/main.go
+++ 80-remove-duplicates-from-sorted-array-ii/main.go
@@ -1,1 +1,35 @@
-package _0_remove_duplicates_from_sorted_array_ii
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(removeDuplicates([]int{0, 0, 1, 1, 1, 1, 2, 3, 3}))
+}
+
+// https://leetcode.cn/problems/remove-duplicates-from-sorted-array-ii/?envType=study-plan-v2&envId=top-interview-150
+func removeDuplicates(nums []int) int {
+	length := len(nums)
+	i := 0
+	flag := false
+	for j := 1; j < length; j++ {
+		if !flag {
+			if nums[i] == nums[j] {
+				i++
+				nums[i] = nums[j]
+				flag = true
+			} else {
+				i++
+				nums[i] = nums[j]
+			}
+		}
+
+		if flag && nums[i] != nums[j] {
+			i++
+			nums[i] = nums[j]
+			flag = false
+		}
+
+	}
+
+	return i + 1
+}
